84bbb516b860bc9214a1fd0cd52bccff08a17932
Sahithi Allegapally <148424406+SahithiA17@users.noreply.github.com>
2024-06-24 20:42:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request
 import pickle
 import requests
 
 app = Flask(__name__)
 
-# Load the data from pickle files
+# Load the data from pickle files (replace with your actual data loading logic)
 movies = pickle.load(open('artifacts/movie_list.pkl', 'rb'))
 similarity = pickle.load(open('artifacts/similarity.pkl', 'rb'))
 
-def fetch_poster(movie_id):
-    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}"
+OMDB_API_KEY = 'YOUR_OMDB_API_KEY'
+
+def fetch_poster(movie_title):
+    url = f"http://www.omdbapi.com/?t={movie_title}&apikey={OMDB_API_KEY}"
     data = requests.get(url).json()
-    poster_path = data.get('poster_path', '')
-    if poster_path:
-        full_path = f"https://image.tmdb.org/t/p/w500/{poster_path}"
-        return full_path
-    return None
+    poster_path = data.get('Poster', '')
+    return poster_path if poster_path != 'N/A' else None
 
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
@@ -23,10 +22,9 @@ def recommend(movie):
     recommended_movie_names = []
     recommended_movie_posters = []
     for i in distances[1:6]:
-        # fetch the movie poster
-        movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movie_names.append(movies.iloc[i[0]].title)
+        movie_title = movies.iloc[i[0]].title
+        recommended_movie_posters.append(fetch_poster(movie_title))
+        recommended_movie_names.append(movie_title)
 
     return recommended_movie_names, recommended_movie_posters
 
@@ -35,13 +33,13 @@ def index():
     movie_list = movies['title'].values
     recommended_movie_names = []
     recommended_movie_posters = []
+    selected_movie = None
 
     if request.method == 'POST':
         selected_movie = request.form.get('movie')
         recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
 
-    return render_template('index.html', movie_list=movie_list, recommended_movie_names=recommended_movie_names, recommended_movie_posters=recommended_movie_posters)
+    return render_template('index.html', movie_list=movie_list, recommended_movie_names=recommended_movie_names, recommended_movie_posters=recommended_movie_posters, selected_movie=selected_movie)
 
 if __name__ == '__main__':
-    # Run the Flask application
     app.run(debug=True)
